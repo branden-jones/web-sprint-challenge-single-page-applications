f8545f912945b52c9d5cb5e3c2a72c1d3910c481
Branden Jones <branden.wayne.jones@gmail.com>
2022-09-26 15:03:17 -0400

    Branden Jones order form, schema, and links created but not working"; git push; " " ""

--- src/App.js
+++ src/App.js
@@ -1,28 +1,137 @@
 import React, { useState, useEffect } from "react";
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Components/Home';
+import OrderForm from "./Components/OrderForm";
+import * as yup from 'yup';
+import schema from './Validation/formSchema'
 
 
-// ~~~link home paA homepage that has a "/" route and links to your form (button, nav bar, or any other type of link is acceptable but must have an id of "order-pizza")
+// ~~~ A homepage that has a "/" route and links to your form (button, nav bar, or any other type of link is acceptable but must have an id of "order-pizza")
 
 // ~~~ A order form that has a "/pizza" route and shows the form
-
 // ~~~ A form with an id of "pizza-form"
+
 //  ~~~ A name text input field with an id of "name-input"
 //  ~~~ Validation for name and the error message is "name must be at least 2 characters" (Use this exact error message to make sure tests pass) ::: VERY IMPORTANT TO USE THAT EXACT ERROR MESSAGE (casing included!)
+
 //  ~~~ A dropdown for pizza size with an id of "size-dropdown"
 // ~~~ A checklist for toppings - at least 4 (hint: name each separately!)
 // ~~~ Text input for special instructions with an id of "special-text"
+
 // ~~~ An Add to Order button that has an id of "order-button" and that submits form and returns a database record of name, size, toppings and special instructions
 
+const initialOrder = {
+  name: '',
+  crust: 'hand-tossed',
+  sauce: 'tomato/marinara',
+  cheese: 'regular',
+  size: '16in',
+  noTop: false,
+  pepperoni: false,
+  onion: false,
+  spinach: false,
+  mushroom: false,
+  pineapple: false,
+  sausage: false,
+  everything: false,
+  special: '',
+
+}
+
+const initialDisabled = true;
+
+
+
 const App = () => {
+
+const [ order, setOrder ] = useState({initialOrder});
+
+const [orderError, setOrderError] = useState('');
+
+const [ disabled, setDisabled ] = useState(initialDisabled)
+
+
+const postNewOrder = newOrder => {
+  console.log(`you forgot to post new order... ding dong!!`, newOrder)
+  // .then(res => {
+    // console.log(res);
+  // })
+  // .catch(err => console.error(err))
+  // .finally(() => setOrder(initialOrder))
+}
+
+  const validate = (name, value) => {
+    yup.reach(schema,name)
+      .validate(value)
+      .then(() => setOrderError({
+        // console.log(`results`, res);
+        ...orderError, [name]: ''}))
+      .catch(err => setOrderError({ ...orderError, [name]: err.errors[0]}))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name,value);
+    setOrder({
+      ...order,
+      [name]: value
+    })
+  }
+
+  const orderSubmit = () => {
+    const newOrder = {
+    name: order.name.trim(),
+    crust: order.crust.trim(),
+    sauce: order.sauce.trim(),
+    cheese: order.cheese.trim(),
+    size: order.size.trim(),
+    toppings: [
+      'noTop',
+      'pepperoni',
+      'onion',
+      'spinach',
+      'mushroom',
+      'pineapple',
+      'sausage',
+      'everything'
+    ].filter(top => !!order[top]),
+    special: order.special
+
+    }
+    postNewOrder(newOrder);
+  }
+
+  useEffect(() => {
+    schema.isValid(order).then(valid => setDisabled(!valid))
+  }, [order])
+
   return (
-    <Switch>
-      <Route path='/'>
-        <Home />
-      </Route>
-        
-    </Switch>
+    <div>
+        <nav className="constant-header">
+          <h1>Lambda Eats</h1>
+          <div className="nav-links">
+            <Link to='/'>Home</Link>
+            <Link to='/pizza'>Order Some Pizza</Link>
+          </div>
+        </nav>
+
+      <Switch>
+       
+        <Route path='/pizza'>
+          <OrderForm 
+            change={inputChange}
+            values={order}
+            error={orderError}
+            submit={orderSubmit}
+            disabled={disabled}
+             />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+
+      </Switch>
+    </div>
   );
 };
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,22 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 export default function Home() {
+    const history = useHistory();
+    const routeToForm = () => {
+        history.push('./pizza');
+    }
 
     return (
         <div id='order-pizza' >
-            <h1>Lambda Eats</h1>
-            <h2>Build Your Own Pizza!</h2>
+            <h1>Build Your Own Pizza!</h1>
             <h4>Cause we only got on person willing to work and they NEED to told how to make a pizza...</h4>
             <img className='home-image' src='https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVwcGVyb25pJTIwcGl6emF8ZW58MHx8MHx8&w=1000&q=80' alt='Delicious Pepperoni Pizza' />
+            <button
+                onClick={routeToForm}
+                className='formBtn'
+            >
+                Build Your Own!
+            </button>
         </div>
     )
 }
